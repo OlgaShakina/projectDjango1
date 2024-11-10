cf81933a6258b48f170d20e9668baddc9aa6f0ab
OlgaShakina <olga.buinova@gmail.com>
2024-11-10 15:02:38 +0300

    add new code

--- zerocoder/news/urls.py
+++ zerocoder/news/urls.py
@@ -5,5 +5,6 @@ from .views import news_detail
 
 urlpatterns = [
     path('', views.home, name='news_home'),
-    path('news/<int:news_id>/', news_detail, name='news_detail')
+    path('news/<int:news_id>/', news_detail, name='news_detail'),
+    path('create_news/', views.create_news, name='add_news')
 ]
--- zerocoder/news/views.py
+++ zerocoder/news/views.py
@@ -9,3 +9,7 @@ def home(request):
 def news_detail(request, news_id):
     news_item = get_object_or_404(NewsPost, id=news_id)
     return render(request, 'news_detail.html', {'news_item': news_item})
+
+
+def create_news(request):
+    return render(request, 'news/add_news.html')
